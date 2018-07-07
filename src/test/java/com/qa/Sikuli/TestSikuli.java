5a0695990387ebabd938f1ef96762d79f27188d5
Rimihere <saggirimi@gmail.com>
2018-07-07 20:26:08 +0530

    First git commit for code

---
+++ src/test/java/com/qa/Sikuli/TestSikuli.java
@@ -0,0 +1,31 @@
+package com.qa.Sikuli;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
+
+public class TestSikuli {
+
+	public static void main(String[] args) throws InterruptedException, FindFailed {
+
+System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Tools\\chromedriver.exe");
+WebDriver driver= new ChromeDriver();
+driver.get("https://www.google.com/");
+//*[@id='gb_70']
+driver.findElement(By.xpath("//*[@id='gb_70']")).click();
+Screen screen=new Screen();
+Pattern createaccount=new Pattern("C:\\Selenium\\Images\\CreateAccount.JPG");
+Thread.sleep(5000);
+screen.click(createaccount);
+
+
+
+
+
+
+	}
+
+}
